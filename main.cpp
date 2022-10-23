a40b07d201f6af90f24e35098cad37eaf1ef20f1
Maciej Mironowicz <m.mironowicz@gmail.com>
2022-10-23 12:45:22 +0200

    Basic movement added

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ class Game
 public:
     Game() : mWindow(sf::VideoMode(800, 600), "Spacefighter"), mPlayer()
     {
-        mPlayer.setRadius(25.f);
+        mPlayer.setRadius(5.f);
         mPlayer.setPosition(150.f, 150.f);
         mPlayer.setFillColor(sf::Color::Red);
     }
@@ -24,13 +24,32 @@ public:
 
 private:
     void processEvents();
+    void handlePlayerInput(sf::Keyboard::Key key, bool isPressed);
     void update();
     void render();
 
 private:
     sf::RenderWindow mWindow;
     sf::CircleShape mPlayer;
+
+    bool mIsMovingUp;
+    bool mIsMovingDown;
+    bool mIsMovingLeft;
+    bool mIsMovingRight;
 };
+
+void Game::handlePlayerInput(sf::Keyboard::Key key, bool isPressed)
+{
+
+    if (key == sf::Keyboard::W)
+        mIsMovingUp = isPressed;
+    if (key == sf::Keyboard::S)
+        mIsMovingDown = isPressed;
+    if (key == sf::Keyboard::A)
+        mIsMovingLeft = isPressed;
+    if (key == sf::Keyboard::D)
+        mIsMovingRight = isPressed;
+}
 
 void Game::processEvents()
 {
@@ -50,7 +69,7 @@ void Game::processEvents()
             handlePlayerInput(event.key.code, false);
             break;
 
-        case sf::Evnet::Closed:
+        case sf::Event::Closed:
             mWindow.close();
             break;
         }
@@ -64,7 +83,21 @@ void Game::render()
     mWindow.display();
 }
 
-void Game::update(){};
+void Game::update()
+{
+
+    sf::Vector2f movement(0.f, 0.f);
+    if (mIsMovingUp)
+        movement.y -= 1.f;
+    if (mIsMovingDown)
+        movement.y += 1.f;
+    if (mIsMovingLeft)
+        movement.x -= 1.f;
+    if (mIsMovingRight)
+        movement.x += 1.f;
+
+    mPlayer.move(movement);
+}
 
 int main()
 {
